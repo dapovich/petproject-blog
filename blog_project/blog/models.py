05d5fd185a4572a7ad9b938f9ce7ef3bfa1fd38f
dap0vich <d4nil.pichugin@yandex.ru>
2023-05-11 18:15:14 +0700

    feat: use canonical URLs for models
    Realize get_absolute_url() function for model Post which allow us to specify the URL for the master
    copy of a page.

--- blog_project/blog/models.py
+++ blog_project/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 # Custom publish manager to retrive posts with PUBLISHED status.
@@ -18,6 +19,7 @@ class Post(models.Model):
 
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
+    # this field defines many-to-one relationship
     author = models.ForeignKey(User,
                                on_delete=models.CASCADE,
                                related_name='blog_posts')
@@ -37,3 +39,7 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[self.id])
